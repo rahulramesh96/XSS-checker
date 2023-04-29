20bcd3e1e2753513cd4a1dc968cfdcbd35b310db
Rahul Ramesh <60769128+rahulramesh96@users.noreply.github.com>
2023-04-29 23:57:07 +0100

    Update xss_checker.py

--- xss_checker.py
+++ xss_checker.py
@@ -4,6 +4,7 @@ import urllib.parse
 import json
 import random
 import string
+import argparse
 
 def generate_payload():
     """
@@ -26,18 +27,22 @@ def generate_payload():
 
     return encoded_payload
 
-def check_xss(url):
+def check_xss(url, auth=None):
     """
     Checks a web application for potential XSS vulnerabilities using advanced payloads.
 
     Args:
         url (str): The URL of the web application.
+        auth (tuple): A tuple containing the user ID and password for authentication. Default: None (unauthenticated scan)
 
     Returns:
         None. Prints any potential vulnerabilities found.
     """
-    # Send a GET request to the URL
-    response = requests.get(url)
+    # Send a GET request to the URL with authentication headers if provided
+    session = requests.Session()
+    if auth is not None:
+        session.auth = auth
+    response = session.get(url)
 
     # Check the response headers and content for potential XSS vulnerabilities
     if 'Content-Security-Policy' not in response.headers:
@@ -55,26 +60,48 @@ def check_xss(url):
         for input_tag in inputs:
             input_name = re.search(r"name=['\"](.*?)['\"]", input_tag).group(1)
             data[input_name] = generate_payload()
-        response = requests.post(url, data=data)
+        response = session.post(url, data=data)
         for payload in data.values():
             if payload in response.text:
                 print(f"Possible reflected XSS vulnerability found in {url} with payload {payload}")
+                print("Remediation: Use proper output encoding to prevent untrusted data from being interpreted as code.")
+
+    # Scan all links on the page for potential DOM-based XSS vulnerabilities
+    links = re.findall(r"<a .*?>", response.text)
+    for link in links:
+        href = re.search(r"href=['\"](.*?)['\"]", link).group(1)
+        if "javascript:" in href:
+            print(f"Possible DOM-based XSS vulnerability found in {url}: {link}")
+            print("Remediation: Use proper input validation and sanitization to prevent untrusted data from being executed as script.")
+
+    # Check any JavaScript files included in the response for potential vulnerabilities
+    scripts = re.findall(r"<script .*?>.*?</script>", response.text, re.DOTALL)
+    for script in scripts:
+        if "document.cookie" in script or "window.location" in script:
+            print(f"Possible stored XSS vulnerability found in {url}: {script[:50]}...")
+            print("Remediation: Use proper input validation and sanitization to prevent untrusted data from being executed as script.")
+
+    # Check for potential XSS vulnerabilities in any JSON data returned by the application
+    json_data = re.findall(r"{.*?}", response.text)
+    for data in json_data:
+        try:
+            decoded_data = json.loads(data)
+        except:
+            continue
+        for key, value in decoded_data.items():
+            if isinstance(value, str):
+                encoded_value = urllib.parse.quote(value, safe='')
+                attack_strings = [
+                    f"<script>{value}</script>",
+                    f"<img src=x onerror=alert({value})>",
+                    f"'{value}",
+                    f'"{value}"',
+                    f'{value}; alert(1)//',
+                    f'"><svg/onload=alert({value})>'
+                ]
+                for attack in attack_strings:
+                    if attack in data or encoded_value in data:
+                        print(f"Possible reflected XSS vulnerability found in {url} with JSON key {key} and data {data[:50]}...")
 
 if __name__ == "__main__":
-    help_message = """
-    Welcome to the advanced XSS vulnerability checker!
-
-    Usage:
-        python xss_checker.py [url]
-
-    Arguments:
-        url (str): The URL of the web application to test.
-
-    Example usage:
-        python xss_checker.py "http://example.com/myapp"
-    """
-    if len(sys.argv) < 2:
-        print(help_message)
-    else:
-        url = sys.argv[1]
-        check_xss(url)
+    # Set up command line argument
